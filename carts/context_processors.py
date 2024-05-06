300e2d3067e2026c970c9ecacedf5d29f3264b9a
Moustapha Sow <sowmoustapha0730@gmail.com>
2024-05-06 08:06:35 +0000

    Implemente cart functionalities without login

---
+++ carts/context_processors.py
@@ -0,0 +1,19 @@
+from .views import _get_cart_id
+from .models import Cart, CartItem
+
+def counter(request):
+    cart_counter = 0
+    if "admin" in request.path:
+        return {}
+    else:
+        try:
+            # cart = Cart.objects.get(cart_id=_get_cart_id(request))
+            # cart_items = CartItem.objects.all().filter(cart=cart)
+            cart = Cart.objects.filter(cart_id=_get_cart_id(request))
+            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            for item in cart_items:
+                cart_counter += item.quantity
+        except Cart.DoesNotExist:
+            cart_counter = 0
+
+    return dict(cart_counter=cart_counter)
--- carts/models.py
+++ carts/models.py
@@ -12,9 +12,12 @@ class Cart(models.Model):
     
 class CartItem(models.Model):
     product     = models.ForeignKey(Product, on_delete=models.CASCADE)
-    Cart        = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart        = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity    = models.IntegerField()
     is_active   = models.BooleanField(default=True)
 
+    def sub_total(self):
+        return (self.quantity * self.product.price)
+
     def __str__(self):
-        return self.product
+        return str(self.product)
