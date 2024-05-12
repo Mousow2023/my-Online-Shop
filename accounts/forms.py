9348b6a32b37a0e0b63c7b0f45954c97fe90e044
Moustapha Sow <sowmoustapha0730@gmail.com>
2024-05-12 13:08:08 +0000

    Set up registration, implement model form and edit init method

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from typing import Any, Mapping
+from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Enter Your Password"
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Confirm Your Password"
+    }))
+    class Meta:
+        model = Account
+        fields = ["first_name", "last_name", "phone_number", "email", "password"]
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs["placeholder"] = "Your First Name"
+        self.fields["last_name"].widget.attrs["placeholder"] = "Your Last Name"
+        self.fields["email"].widget.attrs["placeholder"] = "Your Email Address"
+        self.fields["phone_number"].widget.attrs["placeholder"] = "Your Phone Number"
+
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
