8a162801a53457f28ca99aee0095ac2513055bbc
Moustapha Sow <sowmoustapha0730@gmail.com>
2024-06-20 09:39:18 +0000

    Store the Rating and Reviews

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, ReviewRating
+from .forms import ReviewForm
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _get_cart_id
@@ -7,6 +8,8 @@ from carts.views import _get_cart_id
 from django.db.models import Q
 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.contrib import messages
+
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
@@ -61,5 +64,34 @@ def search(request):
         "keyword": keyword,
     }
 
-    # testing
     return render(request, "store/store.html", context)
+
+
+def submit_review(request, product_id):
+    url = request.META.get("HTTP_REFERER")
+    if request.method == "POST":
+        try:
+            review = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=review)
+            form.save()
+            messages.success(request, "Thank you for your feedback. Your review has been updated!")
+            
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data["subject"]
+                data.review = form.cleaned_data["review"]
+                data.rating = form.cleaned_data["rating"]
+                data.ip = request.META.get("REMOTE_ADDR")
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, "Thank you for your feedback. Your review has been saved!")
+            
+                return redirect(url)
+    
+    return redirect(url)        
+
