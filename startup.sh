aeceb7643897c7c55889d92ce7de8da70d683a2e
Moustapha SOW <mousow123@gmail.com>
2024-10-31 23:16:31 +0000

    update startup file

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python manage.py collectstatic --noinput && gunicorn --workers 2 moushop.wsgi
+python manage.py collectstatic --noinput
+gunicorn --workers 2 --bind 0.0.0.0:8000 moushop.wsgi:application
